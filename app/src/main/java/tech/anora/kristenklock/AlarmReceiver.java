2312aa7dbadac4137b3924f91d325f746058e723
Collin Makohon <cmakohon@me.com>
2017-04-22 16:32:35 -0400

    alarm now working, cant stop it though

--- app/src/main/java/tech/anora/kristenklock/AlarmReceiver.java
+++ app/src/main/java/tech/anora/kristenklock/AlarmReceiver.java
@@ -2,7 +2,11 @@ package tech.anora.kristenklock;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
 
 /**
  * Created by cmakohon on 4/20/17.
@@ -12,6 +16,12 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.v("TAG", "Alarm has gone off!");
 
+        Uri u = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        Ringtone r = RingtoneManager.getRingtone(context, u);
+        r.play();
     }
+
+
 }
--- app/src/main/java/tech/anora/kristenklock/MainActivity.java
+++ app/src/main/java/tech/anora/kristenklock/MainActivity.java
@@ -1,16 +1,55 @@
 package tech.anora.kristenklock;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     private TimePicker time;
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this.getApplicationContext();
+        alarmMgr = (AlarmManager) this.getSystemService(ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        time = (TimePicker) findViewById(R.id.timePicker);
+    }
+
+    public void setAlarm(View v) {
+        int hour = time.getHour();
+        int minute = time.getMinute();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+
+        //if the alarm is set for a time that has already passed in the
+        //current day, set it for the next day
+        if(System.currentTimeMillis() >= calendar.getTimeInMillis()) {
+            calendar.add(Calendar.DATE, 1);
+        }
+
+        alarmMgr.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), alarmIntent);
+
+        Log.v("TAG", "Hour: " + hour + ", Minute: " + minute);
+        Log.v("TAG", "" + calendar.getTimeInMillis());
     }
 }
