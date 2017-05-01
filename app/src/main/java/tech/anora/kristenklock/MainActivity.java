d3f6798791a6fea69d3b051a6e61a5708306e06f
Sifron Benjamin <sifronbenjamin@gmail.com>
2017-05-01 13:23:06 -0400

    Added Calibration button to main activity

--- app/src/main/java/tech/anora/kristenklock/MainActivity.java
+++ app/src/main/java/tech/anora/kristenklock/MainActivity.java
@@ -1,14 +1,11 @@
 package tech.anora.kristenklock;
 
 import android.app.AlarmManager;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
